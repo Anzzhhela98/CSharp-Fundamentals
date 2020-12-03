b0fb77cc1244fa3b3836c3024bbc0c322f92fa2e
unknown <anzzhhela98@gmail.com>
2020-12-03 22:06:19 +0200

    odule

---
+++ Lists/Lists - Lab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> listOfNum = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+            SumAdjacentNumbers(listOfNum);
+        }
+        static void SumAdjacentNumbers(List<double> listOfNum)
+        {
+            for (int i = 0; i < listOfNum.Count - 1; i++)
+            {
+                if (listOfNum[i] == listOfNum[i + 1])
+                {
+                    listOfNum[i] += listOfNum[i + 1];
+                    listOfNum.RemoveAt(i + 1);
+                    i = -1;
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", listOfNum));
+        }
+    }
+}
---
+++ Methods/Methods - Exercises/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+
+            GetSmallestNumbers(firstNum, secondNum, thirdNum);
+        }
+        static void GetSmallestNumbers(int firstNum, int secondNum, int thirdNum)
+        {
+            int minValue = int.MaxValue;
+
+
+            if (firstNum < minValue)
+            {
+                minValue = firstNum;
+            }
+            if (secondNum < minValue)
+            {
+                minValue = secondNum;
+            }
+            if (thirdNum < minValue)
+            {
+                minValue = thirdNum;
+            }
+
+            Console.WriteLine(minValue);
+        }
+    }
+}
